581aa846ae281473fa628d2598e0830b020800b3
Thonia Senna <thonia@poli.ufrj.br>
2019-09-26 03:26:52 -0300

    adiciona noiseless case e agora amplitudes em módulo

--- pos_quali1.m
+++ pos_quali1.m
@@ -24,12 +24,13 @@ d3= d3./std(d3(:)); %ESTÁ CERTO?
 n              = randn(size(d3));
 n              = n./std(n(:));
     
-    SNR=9;
+    SNR=12;
     %SNR_dB         = 10*log10(SNR);
     
     alpha          = 1./sqrt(SNR);
     n              = alpha*n;
-d3_awgn         = d3+n;
+%d3_awgn         = d3; %sem ruído                                                                                                
+ d3_awgn         = d3+n;
 Nw= length(ricker(f0,dt));
 [Ns, Nx]= size(d3);
 L              = Ns-Nw+1;
@@ -58,7 +59,8 @@ w=v_M'*r;
 % 1 amplitudes dos autovalores
 x1=1:length(d_M);
 figure(1), 
-plot(x1,diag(d_M)), hold on
+plot(x1,abs(diag(d_M))), hold on
+%title(['Magnitudes dos autovalores referentes ao dado sem ruído'])
 title(['Magnitudes dos Autovalores referentes ao SNR= ',num2str(SNR)])
 xlim([x1(1) x1(end)]), grid on
 xlabel('Autovalor')
@@ -71,6 +73,7 @@ for vv=1:length(v_M);
 end
 figure(2), 
 plot(x1,normas,'.'), hold on
+%title(['Normas L1 dos Autovetores referentes dado sem ruído'])
 title(['Normas L1 dos Autovetores referentes ao SNR= ',num2str(SNR)])
 xlim([x1(1) x1(end)]), grid on
 xlabel('Autovetor')
@@ -78,11 +81,12 @@ ylabel('Norma L1')
 %%% 3- com as amplitudes de w
 %% 
 figure(3)
-plot(x1,w)
+plot(x1,abs(w),'.')
 hold on
+%title(['Análise de Componentes do vetor w referentes ao dado sem ruído'])
 title(['Análise de Componentes do vetor w referentes ao SNR= ',num2str(SNR)])
 xlabel('Autovetor')
-ylabel('Constante correspondente')
+ylabel('Constante correspondente (abs)')
 %% ATÉ AQUI
 %%%%%%%%%%
 %%%%%%%%%%
@@ -91,6 +95,8 @@ ylabel('Constante correspondente')
 figure(4)
 subplot(121), wigb(dr), title('Refletividades Originais')
 subplot(122), wigb(d3_awgn), title('Traços com Ruído')
+%subplot(122), wigb(d3_awgn), title('Traços sem Ruído')
+%suptitle(['Para a simulação do dado sem ruído'])
 suptitle(['Para a simulação do SNR= ',num2str(SNR)])
 % 6 - O resultado obtido com o "melhor" autovetor E 
 % 7 - O resultado obtido com o Wiener 
@@ -115,6 +121,7 @@ for ii =1:length(d_M)
 figure(5)
 plot(x1,pcorr,'.')
 hold on
+%title(['PCC de cada autovetor em relação à refletividade original referente ao dado sem ruído '])
 title(['PCC de cada autovetor em relação à refletividade real referente ao SNR= ',num2str(SNR)])
 xlabel('Autovetor')
 ylabel('PCC')
@@ -129,6 +136,9 @@ pcorr_w = w_otim'*r/(norm(w_otim)*norm(r));
 figure(6)
 y = [melhorp pcorr_w];
 barh(y)
+%title(['Sem ruído'])
+title(['PCC de w (em cima) e PCC do melhor autovetor (embaixo) em relação à refletividade original. SNR= ',num2str(SNR)])
+
 yticklabels({'Melhor Autovalor','W Calculado'})
 xlabel('PCC')
 ylabel('Metodo')
